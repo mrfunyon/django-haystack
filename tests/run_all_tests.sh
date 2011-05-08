3f27fa4bcc3e58efcd02ceb86fa343ab44e56c31
Daniel Lindsley <daniel@toastdriven.com>
2011-05-08 12:25:24 -0500

    All old tests passing! Just need some multi-index tests & docs...

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -3,6 +3,14 @@ echo "** CORE **"
 django-admin.py test core --settings=settings
 echo ""
 
+echo "** DISCOVERY **"
+django-admin.py test discovery --settings=discovery_settings
+echo ""
+
+echo "** OVERRIDES **"
+django-admin.py test overrides --settings=overrides_settings
+echo ""
+
 echo "** SIMPLE **"
 django-admin.py test simple_tests --settings=simple_settings
 echo ""
@@ -13,11 +21,4 @@ echo ""
 
 echo "** WHOOSH **"
 django-admin.py test whoosh_tests --settings=whoosh_settings
-echo ""
-
-echo "** SITE REG **"
-django-admin.py test site_registration --settings=site_registration_settings
-
-echo "** OVERRIDES **"
-django-admin.py test overrides --settings=overrides_settings
 echo ""
